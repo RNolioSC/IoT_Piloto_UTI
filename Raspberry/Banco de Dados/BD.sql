4799eaad977933d693405e5d90e3aadd6c6cf502
RNolioSC <rnoliosc@gmail.com>
2023-03-20 19:15:32 -0300

    Implementação do BD no raspberry

--- Raspberry/Banco de Dados/BD.sql
+++ Raspberry/Banco de Dados/BD.sql
@@ -1,49 +1,49 @@
-CREATE DATABASE "BancoDeDados"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+--CREATE DATABASE "BancoDeDados"
+--    WITH
+--    OWNER = postgres
+--    ENCODING = 'UTF8'
+--    CONNECTION LIMIT = -1
+--    IS_TEMPLATE = False;
 
 DROP TABLE IF EXISTS PacienteDiagnostico;
-DROP TABLE IF EXISTS Pacientes;
 DROP TABLE IF EXISTS Diagnosticos;
-DROP TABLE IF EXISTS AtendimentoEnfermeiro;
-DROP TABLE IF EXISTS Enfermeiros;
-DROP TABLE IF EXISTS AtendimentoTecnicoEnf;
-DROP TABLE IF EXISTS TecnicosEnf;
+DROP TABLE IF EXISTS AtendimentoProfEnf;
+DROP TABLE IF EXISTS ProfissionaisEnf;
 DROP TABLE IF EXISTS Atendimentos;
 DROP TABLE IF EXISTS AtividadesNAS;
+DROP TABLE IF EXISTS Pacientes;
 
 CREATE TABLE Pacientes (codigo SERIAL PRIMARY KEY, nome VARCHAR(50)); 
 
 CREATE TABLE Diagnosticos(codigo SERIAL PRIMARY KEY, nome VARCHAR(50));
 
+-- BIGINT UNSIGNED por compatibilidade com mariadb
 CREATE TABLE PacienteDiagnostico(
-	codPaciente int,
-	codDiagnostico int,
+	codPaciente BIGINT UNSIGNED, 
+	codDiagnostico BIGINT UNSIGNED,
 	PRIMARY KEY(codPaciente, codDiagnostico),
-	CONSTRAINT fk1 FOREIGN KEY (codPaciente) REFERENCES Pacientes(codigo) ,
+	CONSTRAINT fk1 FOREIGN KEY (codPaciente) REFERENCES Pacientes(codigo),
 	CONSTRAINT fk2 FOREIGN KEY (codDiagnostico) REFERENCES Diagnosticos(codigo)
 );
 
-CREATE TABLE AtividadesNAS(codigoNAS VARCHAR(2) PRIMARY KEY, descricao VARCHAR(500), pontos float);
+-- indice pra sort by
+CREATE TABLE AtividadesNAS(codigoNAS VARCHAR(2) PRIMARY KEY, descricao VARCHAR(500), pontos FLOAT, indice SERIAL);
 
 
 CREATE TABLE Atendimentos (codigo SERIAL PRIMARY KEY, diaHora TIMESTAMP, 
 						   atividade VARCHAR(2),
-						   paciente int,
+						   paciente BIGINT UNSIGNED,
 						   FOREIGN KEY (atividade) REFERENCES AtividadesNAS(codigoNAS),
 						   FOREIGN KEY (paciente) REFERENCES Pacientes(codigo)
 						   );
 
 
-CREATE TABLE profissionaisEnf (codigo VARCHAR(8) PRIMARY KEY, nome VARCHAR(50), tipo char);
+CREATE TABLE ProfissionaisEnf (codigo VARCHAR(24) PRIMARY KEY, nome VARCHAR(50), tipo char);
 
 CREATE TABLE AtendimentoProfEnf(
-	codAtendimento int,
-	codProfEnf VARCHAR(8),
+	codAtendimento BIGINT UNSIGNED,
+	codProfEnf VARCHAR(24),
 	PRIMARY KEY (codAtendimento, codProfEnf),
 	FOREIGN KEY (codAtendimento) REFERENCES Atendimentos(codigo),
-	FOREIGN KEY (codProfEnf) REFERENCES profissionaisEnf(codigo)
+	FOREIGN KEY (codProfEnf) REFERENCES ProfissionaisEnf(codigo)
 );
--- Raspberry/leitorSerial.py
+++ Raspberry/leitorSerial.py
@@ -3,13 +3,37 @@ Mostra valores da antena
 '''
 
 import serial
+import mysql.connector
 
-ser = serial.Serial('/dev/ttyUSB0')
+def write_to_db(tag):
+        pass
 
-while True:
-    if ser.in_waiting:
-        bs = ser.read(ser.in_waiting)
+if __name__ == "__main__":
 
-        bs = '/'.join([f'0x{b:x}' for b in bs])
-        print(bs)
+        ser = serial.Serial('/dev/ttyUSB0')
+
+        while True:
+            if ser.in_waiting:
+                #print("connected")
+                bs = ser.read(ser.in_waiting)
+                #print(bs)
+
+                bs = '/'.join([f'0x{b:x}' for b in bs])
+                #print(bs)
+                bs = bs.removeprefix('0xcc/0xff/0xff/0x10/0x32/0xd/0x1/')
+                lastchar = bs[-1]
+                while lastchar != '/':
+                        bs = bs[:-1]
+                        lastchar = bs[-1]
+                bs = bs[:-1] # remove '/'
+                print(bs)
+                bs = bs.removeprefix('0x')
+                temp = bs.split('/0x')
+                tag = ''
+                for i in temp:
+                        if len(i)<2:
+                                tag+='0'
+                        tag += i
+                print(tag)
+                write_to_db(tag)
 
